458a06615bca7c2d711e523fa8fc05b2cb7fb899
AsafWeitzman <asafweit1@gmail.com>
2023-01-23 20:41:24 +0200

    play with the data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [listOfInterviews, setListOfInterviews] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/Interviews").then((response) => {
+      console.log(response.data);
+      const interviews = response.data;
+      setListOfInterviews(interviews);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      {listOfInterviews.map((value, key) => (
+        <li key={key}>{value.jobTitle}</li>
+      ))}
+    </div>
+  );
 }
 
 export default App;
